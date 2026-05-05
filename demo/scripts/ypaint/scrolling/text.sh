c0641b8dcf312759063a1696d231711278d6cd00
zokrezyl <zokrezyl@gmail.com>
2026-05-05 14:41:55 +0200

    presentation demo

--- demo/scripts/ypaint/scrolling/text.sh
+++ demo/scripts/ypaint/scrolling/text.sh
@@ -5,19 +5,24 @@
 YAML='body:
   - text:
       position: [50, 50]
-      content: "Hello YPaint!"
+      content: "Hello World!"
       font-size: 48
       color: "#ff0000"
   - text:
       position: [50, 100]
-      content: "This is MSDF text rendering with_underscore_test"
+      content: "Wellcome to this presentation about some features of the Yetty Terminal Unchained"
       font-size: 16
       color: "#00ff00"
   - text:
       position: [50, 130]
-      content: "Multiple lines of text"
+      content: "This is a rich text rendered on gpu using MSDF fonts"
       font-size: 14
-      color: "#0000ff"
+      color: "#00ffff"
+  - text:
+      position: [0, 160]
+      content: "Yetty stands for (Ye)tty(tty). You likely know what tty means"
+      font-size: 14
+      color: "#00ffff"
 '
 
 # Base64 encode the YAML
