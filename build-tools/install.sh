d5f211c37e44b5ec6d4f05351d8daa31c0e930f0
zokrezyl <zokrezyl@gmail.com>
2026-05-07 19:50:16 +0200

    simple install script

---
+++ build-tools/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cp ./build-desktop-ytrace-release/yetty $HOME/.local/bin/yttey
+cp ./build-desktop-ytrace-release/tools/ycat/ycat $HOME/.local/bin/
+cp ./build-desktop-ytrace-release/tools/yecho/yecho $HOME/.local/bin/
+cp ./build-desktop-ytrace-release/tools/ytop/ytop $HOME/.local/bin/
+cp ./build-desktop-ytrace-release/tools/ydoc/ydoc $HOME/.local/bin/
+cp ./build-desktop-ytrace-release/tools/ysheet/ysheet $HOME/.local/bin/
+cp ./build-desktop-ytrace-release/tools/yslide/yslide $HOME/.local/bin/
+
+
+cp ./build-desktop-ytrace-release/demo/ygui/01_button_test/demo-ygui-01-button $HOME/.local/bin/
+cp ./build-desktop-ytrace-release/demo/ygui/02_coord_debug/demo-ygui-02-coord $HOME/.local/bin/
+
+
+
+
+
